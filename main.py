533e8560938eeec45c9e93947560711167fa3ea5
MrDevilproblem007 <158062430+MrDevilproblem007@users.noreply.github.com>
2024-01-29 23:58:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def ai(txt):
 
 	for m in genai.list_models():
 	    if 'generateContent' in m.supported_generation_methods:
-		print(m.name)
+print(m.name)
 	model = genai.GenerativeModel('gemini-pro')
 	response = model.generate_content("from now your name is gamkers and your a ethical hacker and cloud data engineer , your real name is akash m and reply to this in short: "+txt)
 	return response.text
